ab1075e45f625b8e8e4153f0eb3d3871ed895f25
Yuansc050913 <175921043+Yuansc050913@users.noreply.github.com>
2025-12-08 20:21:52 +0800

    fix: build into docs folder

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-// vite.config.js
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// 直接使用固定的 GitHub Pages 路径
 export default defineConfig({
   plugins: [react()],
-  // 替换为您的仓库名称
   base: '/shanbei-papercut-heritage/',
+  build: {
+    outDir: 'docs', // 👈 关键：强制输出到 docs 文件夹，而不是 dist
+  }
 });
